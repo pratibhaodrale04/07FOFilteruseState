3dcc68aedd5d11403f93d3d589d68b0e4ee2a4b3
pratibhaodrale04 <162298239+pratibhaodrale04@users.noreply.github.com>
2024-06-15 15:07:21 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const APP_LOGO_IMG_URL = "https://maayaofbath.co.uk/wp-content/uploads/2022/02/maya-logo.svg";
+export const APP_LOGO_IMG_URL = "";
 
-export const REST_CARD_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const REST_CARD_IMG_URL = "";
